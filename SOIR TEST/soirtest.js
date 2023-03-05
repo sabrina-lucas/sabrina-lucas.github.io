34cecb397a3378e11eb76573fcd0dbdaf94e9cd2
Sabrina LUCAS <123946374+sabrina-lucas@users.noreply.github.com>
2023-03-05 21:31:57 +0100

    MON SITE WEB TEST

---
+++ SOIR TEST/soirtest.js
@@ -0,0 +1,12 @@
+const prenom ="clark";
+console.log(prenom);
+
+
+const notes = [10, 12, 15, 15, 20];
+console.log(notes);
+
+alert('Bonsoir tout le monde !');
+
+const name = prompt('Quel est ton nom?', 'tape ton nom');
+
+const confirmation = confirm ('etes-vous sur de quitter la partie?');
